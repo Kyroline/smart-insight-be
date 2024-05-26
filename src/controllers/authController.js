59cd5a3fb68109dc12548ecede20e1816a184f65
Kyroline <rizky.w.dewantoro@gmail.com>
2024-05-26 16:15:59 +0700

    Fixed crash on error in auth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,22 +1,29 @@
 import bcrypt from 'bcrypt'
+import { ErrorResponse } from '../utils/errors.js'
 import User from '../models/User.js'
 import { generateToken, validateToken } from '../utils/jwt.js'
 
-export const login = async (req, res) => {
-    const user = await User.findOne({ email: req.body.email })
+export const login = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ email: req.body.email }).select('+password')
+        if (!user)
+            throw new ErrorResponse(401, 'Unauthenticated')
 
-    let match = await bcrypt.compare(req.body.password, user.password)
+        let match = await bcrypt.compare(req.body.password, user.password)
 
-    let userJson = user.toJSON()
-    if (match) {
+        let userJson = user.toJSON()
+        if (!match)
+            throw new ErrorResponse(401, 'Unauthenticated')
+
         delete userJson.password
-        res.json({ token: generateToken(userJson) })
-    } else {
-        res.status(401).json({ message: 'Unauthenticated' })
+        
+        return res.json({ user: userJson, token: generateToken(userJson) })
+    } catch (error) {
+        next(error)
     }
 }
 
-export const register = async (req, res) => {
+export const register = async (req, res, next) => {
     let password = await bcrypt.hash(req.body.password, 10)
 
     const user = await User.create({
@@ -27,13 +34,18 @@ export const register = async (req, res) => {
     })
 
     let userJson = user.toJSON()
+
     delete userJson.password
     res.json({ user: userJson, token: generateToken(userJson) })
 }
 
-export const validate = async (req, res) => {
-    let [type, token] = req.headers['authorization'].split(' ')
+export const validate = async (req, res, next) => {
+    try {
+        let [type, token] = req.headers['authorization'].split(' ')
 
-    let user = await validateToken(token)
-    res.json({ data: user })
+        let user = await validateToken(token)
+        res.json({ data: user })
+    } catch (error) {
+        next(error)
+    }
 }
